7875ff1d905fb782233e94c77061f873879d40c4
Harsha Podapati <harshapodapati@10-5-220-172.vlan876.client.wireless.uic.edu>
2019-07-14 22:59:33 -0500

    lower

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 
-from env.SingleMaze import NewMaze
+from env.SingleMaze import newMaze
 
 import pandas as pd
 walls = {2, 5, 12, 13}
