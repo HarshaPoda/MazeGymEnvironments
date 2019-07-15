1eb1e1bd88ab929907b90d4127db4a62e0118fc3
Harsha Podapati <harshapodapati@10-5-220-172.vlan876.client.wireless.uic.edu>
2019-07-14 22:55:25 -0500

    Added new testing environment

---
+++ env/SingleMaze.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import gym
+from gym import error, spaces, utils
+
+class newMaze(gym.Env):
+    """A new Maze for the gym environment"""
+    metadata = {'render.modes': ['human']}
+    
+    def __init__(self, m):
+        super(NewMaze, self).__init__()
+        self.m = m
+        self.action_space = spaces.Discrete(2)
+        self.observation_space = spaces.Discrete(5)
+        self.state = np.zeros(5) 
+        self.agentPosition = 0
+        self.numSteps = 0
+        
+    def step(self, action):
+        
+        if action == 0 and (agentPosition==0):
+            self.agentPosition = self.agentPosition
+            reward = -2      
+        elif action == 0:
+            self.agentPosition = self.agentPosition - 1
+            reward = -1
+            
+        if action == 0:
+            self.agentPosition = self.agentPosition
+            reward = 0
+        if action == 0:
+            self.agentPosition = self.agentPosition + 1  
+            reward = 1
+        self.numSteps+=1
+        if (self.agentPosition==4):
+            done=True
+        else:
+            done=False
+        return self.agentPosition,reward, done, None
+    
+    def reset(self):
+        self.agentPosition = 0
+        self.state = np.zeros(5)
+        return self.agentPosition
+    
+    def render(self):
+        print("Printing Board")
+        for row in self.state:
+            print('- ')
+        
--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 
-from env.NewGymMaze import TestWorld
+from env.SingleMaze import NewMaze
 
 import pandas as pd
 walls = {2, 5, 12, 13}
-env = DummyVecEnv([lambda: TestWorld(3,5,walls)])
-
+#env = DummyVecEnv([lambda: TestWorld(3,5,walls)])
+env = DummyVecEnv([lambda: newMaze(5)])
 model = PPO2(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=20000)
 
